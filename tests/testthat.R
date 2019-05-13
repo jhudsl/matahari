f79ce2216f96a26f7580ce8ae68581537f08027f
seankross <smk240@gmail.com>
2019-05-13 12:24:08 -0700

    added dance_recital test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
 library(matahari)
+library(purrr)
 
 test_check("matahari")
---
+++ tests/testthat/test_dance_recital.R
@@ -0,0 +1,36 @@
+context("Test that dance_recital sanely evaluates R code")
+
+code_file <- system.file("test", "sample_code.R", package = "matahari")
+
+# # Create file to compare
+# code_file %>%
+#   dance_recital() %>%
+#   saveRDS(file = file.path("inst", "test", "code_file_data.rds"))
+
+recital_result <- code_file %>%
+  dance_recital()
+
+expected_result <- readRDS(system.file("test", "code_file_data.rds",
+                                       package = "matahari"))
+
+test_that("dance_recital can read a code file", {
+  expect_equal(purrr::flatten(recital_result), purrr::flatten(expected_result))
+})
+
+code_string <- "set.seed(42);x <- sample(1:10, 5);median(x);y"
+
+# # Create file to compare
+# code_string %>%
+#   dance_recital() %>%
+#   saveRDS(file = file.path("inst", "test", "code_string_data.rds"))
+
+expected_string_result <- readRDS(system.file("test", "code_string_data.rds",
+                                              package = "matahari"))
+
+recital_string_result <- code_string %>%
+  dance_recital()
+
+test_that("dance_recital can read a code string", {
+  expect_equal(purrr::flatten(recital_string_result),
+               purrr::flatten(expected_string_result))
+})
