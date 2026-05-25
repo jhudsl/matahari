64baede049d4d14ca8b36b465ba727ba07fc6f62
seankross <smk240@gmail.com>
2026-05-25 13:39:09 -0400

    dance recital fix v3

--- R/dance.R
+++ R/dance.R
@@ -297,8 +297,8 @@ dance_report <- function(...) {
 #' ## |stop("Error!")      |NULL     |list(message = "Error!", call = .f(...)) |
 #' ## |warning("Warning!") |Warning! |NULL                                     |
 #' ## |message("Hello?")   |NULL     |NULL                                     |
-#' ## |cat("Welcome!")     |NULL     |NULL   
-#' }                                  |
+#' ## |cat("Welcome!")     |NULL     |NULL                                     |
+#' }
 dance_recital <- function(code, evaluate = TRUE) {
   file_exists <- file.exists(code)
 
